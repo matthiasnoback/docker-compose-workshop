5d053a44a47c12187e0cda46bd45e662baa1b7fa
Matthias Noback <matthiasnoback@gmail.com>
2017-05-10 08:25:50 +0200

    Rollback changes for assignment

--- web/index.php
+++ web/index.php
@@ -6,4 +6,4 @@ echo "Hello, World!\n";
 
 $redis = new Redis();
 $redis->connect('redis');
-echo 'Number of visits: ' . $redis->incrBy('visitors', random_int(1, 3)) . "\n";
+echo 'Number of visits: ' . $redis->incr('visitors') . "\n";
