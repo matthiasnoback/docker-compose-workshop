512fec8451b3f96fb84bfbcdd2e5d8ac61834451
Matthias Noback <matthiasnoback@gmail.com>
2017-10-12 13:55:25 +0200

    A working solution

--- web/index.php
+++ web/index.php
@@ -6,4 +6,4 @@ echo "Hello, World!\n";
 
 $redis = new Redis();
 $redis->connect('redis');
-echo 'Number of visits: ' . $redis->incr('visitors') . "\n";
+echo 'Number of visits: ' . $redis->incrBy('visitors', mt_rand(1, 4)) . "\n";
